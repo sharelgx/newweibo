f01496f9a4dc1d7eee1046727e9b5631a9bd1935
Liu guangxin <7132699@qq.com>
2019-10-20 15:16:35 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -11,21 +11,26 @@ class SessionsController extends Controller
         return view('sessions.create');
     }
 
-    public function store(Resquest $resquest){
-        $credentials = $this->validate($resquest, [
-            'email' => 'required|email|max:255',
-            'password' => 'required'
-        ]);
+    public function store(Request $request)
+    {
+       $credentials = $this->validate($request, [
+           'email' => 'required|email|max:255',
+           'password' => 'required'
+       ]);
+
+       if (Auth::attempt($credentials)) {
+           session()->flash('success', '欢迎回来！');
+           return redirect()->route('users.show', [Auth::user()]);
+       } else {
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back()->withInput();
+       }
+    }
+    public function destroy(){
         
-        if(Auth::attempt($credentials))
-        {
-            // 登陆成功后的相关操作
-            session()->flash('success', '欢迎回来！');
-            return redirect()->route('users.show', [Auth::user()]);
-        }else{
-            // 登录失败后的相关操作
-            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-            return redirect()->back()->withInput();
-        }
+        
+        Auth::logout();
+        session()->flash('success', '您已经成功退出！');
+        return redirect('login');
     }
 }
