9f7475b4d0e11256cf4c31d35cf3c90dfde44d52
Liu guangxin <7132699@qq.com>
2019-10-22 07:12:30 +0000

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,7 +6,16 @@ use Illuminate\Http\Request;
 use App\User;
 use Auth;
 class UsersController extends Controller
-{
+{   
+    public function __construct(){
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store',]
+        ]);
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+
+    }
     public function create(){
         return view('users.create');
     }
@@ -14,6 +23,7 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
     public function store(Request $request){
+        //验证并注册
         $this->validate($request, [
             'name' => 'required|max:50',
             'email'=> 'required|email|unique:users|max:255',
@@ -31,9 +41,11 @@ class UsersController extends Controller
         return redirect()->route('users.show', [$user]);
     }
     public function edit(User $user){
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
     public function update(User $user, Request $request){
+        $this->authorize('update', $user);
         $this->validate($request, [
             'name' =>'required|max:50',
             'password' => 'nullable|confirmed|min:6'
