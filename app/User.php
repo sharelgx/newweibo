579ddeebb28bc43987f41f3e3ba6b2578845b4b9
Liu guangxin <7132699@qq.com>
2019-10-24 04:11:08 +0000

    关注用户动态流

--- app/User.php
+++ app/User.php
@@ -6,6 +6,8 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Auth;
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -54,8 +56,9 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
     public function feed(){
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)->with('user')->orderBy('created_at', 'desc');
     }
     //获取粉丝
     public function followers(){
